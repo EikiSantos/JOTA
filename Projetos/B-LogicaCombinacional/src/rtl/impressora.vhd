7c041557b204deac2abadd4fd34774afebbfab7b
Carlos <cadudip@hotmail.com>
2020-02-29 10:08:08 -0300

    Finished printer.

--- Projetos/B-LogicaCombinacional/src/rtl/impressora.vhd
+++ Projetos/B-LogicaCombinacional/src/rtl/impressora.vhd
@@ -10,5 +10,5 @@ end entity;
 architecture arch of impressora is
 
 begin
-    x <= (SW2 and (SW1 or ((not SW1) and SW4))) or (SW3 and ((SW1 xor SW4) or (SW2 and not SW4))) -- B(A + A_D) + C(A(+)D + BD_)
+    x <= (SW2 and (SW1 or ((not SW1) and SW4))) or (SW3 and ((SW1 xor SW4) or (SW2 and not SW4))); -- B(A + A_D) + C(A(+)D + BD_)
 end architecture;
